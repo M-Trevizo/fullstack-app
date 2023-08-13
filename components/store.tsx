05ea5bbf6a325943932a602b9011553fd249fd92
Michael Trevizo <michael.trevizo93@yahoo.com>
2023-08-13 14:37:46 -0700

    Export rootstate type.

--- components/store.tsx
+++ components/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import formSlice from "./formSlice";
 
-export default configureStore({
+export const store = configureStore({
     reducer: {
-        form: formSlice
+         formSlice
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
