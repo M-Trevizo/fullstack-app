0b91803ad2181eeea5eb0405ae2d7c73714712f2
Michael Trevizo <michael.trevizo93@yahoo.com>
2023-08-17 01:12:16 -0700

    Add function for getting current user

--- src/pb/pocketbase.ts
+++ src/pb/pocketbase.ts
@@ -1,3 +1,7 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase(process.env.DB_HOST);
+export const pb = new PocketBase(process.env.EXPO_PUBLIC_DB_HOST);
+
+export const currentUser = () => {
+    return pb.authStore.model;
+}
